798a4834090a5fe6225be1e67cd6f09378fcf677
niushuai <niushuaiml@gmail.com>
2017-01-18 00:23:15 +0800

    集成spring-data-jpa

---
+++ src/main/java/info/niushuai/dao/UserRepository.java
@@ -0,0 +1,21 @@
+package info.niushuai.dao;
+
+import info.niushuai.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * Created by niushuai on 16/01/2017.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByName(String name);
+
+    User findByNameAndAge(String name, Integer age);
+
+    @Query("from User u where u.name=:name")
+    User findUser(@Param("name") String name);
+
+
+}
